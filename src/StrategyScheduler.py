afa9e3b9d2bf4aae154471c50ccb2803418079be
Wouter Geraedts <git@woutergeraedts.nl>
2014-04-15 10:00:45 +0200

    StrategySchedule: time_left is divided fairly now. (Slightly worse, less overfit (?) 96.26 % / 94.2104 %)

--- src/StrategyScheduler.py
+++ src/StrategyScheduler.py
@@ -8,15 +8,16 @@ from MLiP_eval import StrategyScheduleScore
 class StrategyScheduler(object):
     classifiers = []
     models = []
+    
     attributeNames = []
     yNames = []
     
-    failure_time = 600.0
     total_time = 300.0
     time_modifier = 1.20
 
     def __init__(self):
         pass
+    
     def fit(self, filename):
         X = []
         ys = []
@@ -57,19 +58,14 @@ class StrategyScheduler(object):
             self.models.append(model)
            
         pass
-    def predict(self,features):
-        prediction_tuples = []
-        for i in range(len(self.models)):
-            if self.classifiers[i].predict(features):
-                prediction_tuples.append((self.yNames[i], self.models[i].predict(features)))
-        prediction_tuples.sort(key=lambda x: x[1])
-        
-        if len(prediction_tuples) == 0: # Got no viable solution
-            return 'NewStrategy101164:150.0,NewStrategy101980:150.0' # Just try something
-        
+    
+    def schedule(self, prediction_tuples):
         time_left = self.total_time
         strategies = []
         for name, time in prediction_tuples:
+            if len(strategies) == 20:
+                break;
+            
             if time < 0.0: # Faster than 0.0 sec should be a good strategy (note that -1 is filtered out above)
                 time = 1.0;
         
@@ -83,7 +79,22 @@ class StrategyScheduler(object):
             strategies.append((name, time))
             time_left -= time
         
-        strategies[0] = (strategies[0][0], strategies[0][1] + time_left) # Give time left to best strategy
+        for i in range(len(strategies)):
+            strategies[i] = (strategies[i][0], strategies[i][1] + time_left / len(strategies))
+        
+        return strategies
+        
+    def predict(self, features):
+        prediction_tuples = []
+        for i in range(len(self.models)):
+            if self.classifiers[i].predict(features):
+                prediction_tuples.append((self.yNames[i], self.models[i].predict(features)))
+        prediction_tuples.sort(key=lambda x: x[1])
+        
+        if len(prediction_tuples) == 0: # Got no viable solution
+            return 'NewStrategy101164:150.0,NewStrategy101980:150.0' # Just try something
+        
+        strategies = self.schedule(prediction_tuples)
     
         return ",".join(['%s:%f' % strategy for strategy in strategies])
 
